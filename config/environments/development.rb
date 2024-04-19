d25aca6f356520c71db6f1bd9ffef24f0ca47566
devFelipeAlmeida <felipelongo321@gmail.com>
2024-04-19 16:58:34 -0300

    forgot password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,18 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.enable_reloading = true
 
+  config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address:         'smtp.gmail.com',
+  port:            587,
+  domain:          'localhost',
+  user_name:       'felipealmeida504@gmail.com',
+  password:        'jpqh tjao dkgx tjxr',
+  authentication:  'plain',
+  enable_starttls: true,
+  open_timeout:    5,
+  read_timeout:    5 }
+
   # Do not eager load code on boot.
   config.eager_load = false
 
@@ -37,7 +49,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -73,4 +85,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,10 @@ Rails.application.routes.draw do
 
   resources :comments, only: [:create, :destroy]
   resources :guest_comments, only: [:create, :destroy]
+
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
 end
