a5a0fc1a96275bef97001f807e24851e3b9a0cfb
devFelipeAlmeida <felipelongo321@gmail.com>
2024-04-20 13:17:22 -0300

    ultimas alterações

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :full_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :full_name, :profile_pic, :bio])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_18_183543) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_19_201431) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -88,6 +88,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_18_183543) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
