2a06922f232375ef538c749aad3b2f89cd589f39
devFelipeAlmeida <felipelongo321@gmail.com>
2024-04-22 23:53:30 -0300

    last db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema[7.1].define(version: 2024_04_21_123805) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "adminpack"
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
